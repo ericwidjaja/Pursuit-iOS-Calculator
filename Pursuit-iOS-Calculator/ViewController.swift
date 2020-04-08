17d39438d3cdf54ac6d1b37412c254fa8798c604
Eric Widjaja <ericwidjaja@pursuit.org>
2020-04-08 19:53:15 -0400

    create a function to clear the display and reset numbers to 0, when AC button is pressed

--- Pursuit-iOS-Calculator/ViewController.swift
+++ Pursuit-iOS-Calculator/ViewController.swift
@@ -1,28 +1,58 @@
-//
 //  ViewController.swift
 //  Pursuit-iOS-Calculator
-//
 //  Created by Eric Widjaja on 4/7/20.
 //  Copyright © 2020 Eric Widjaja. All rights reserved.
-//
+
+/*  12 + 23 = 35?
+ previousNumbers
+ numbersOnDisplay
+ isPerformingOperation
+ operation +, -, /, *
+ */
 
 import UIKit
 
 class ViewController: UIViewController {
     
+    //MARK: - Properties:
+    var numbersOnDisplay: Double = 0
+    var previousNumbers: Double = 0
+    var operation = 0
     
     
     @IBOutlet weak var resultLabel: UILabel!
     
     @IBAction func numbers(_ sender: UIButton) {
         
-        let selectedTag = sender.tag
-        resultLabel.text = String(selectedTag - 1)
+        let numbersTag = sender.tag
+        resultLabel.text = resultLabel.text! + String(numbersTag - 1)
     }
+    
+    
+    @IBAction func operatorButtonPressed(_ sender: UIButton) {
+        
+        let operatorsTag = sender.tag
+        
+        //AC = All Clear button pressed
+        if operatorsTag == 11 {
+            resultLabel.text = String(0)
+            numbersOnDisplay = 0
+            previousNumbers = 0
+            operation = 0
+            
+            
+        }
+        
+        
+        
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = #colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1)
+        resultLabel.text = ""
     }
 
 
