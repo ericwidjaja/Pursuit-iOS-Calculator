93731bd0b838d8f56c74b32a3f49abf56bc1c7dd
Eric Widjaja <ericwidjaja@pursuit.org>
2020-04-07 23:16:14 -0400

    add gitignore file

---
+++ Pursuit-iOS-CalculatorTests/Pursuit_iOS_CalculatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  Pursuit_iOS_CalculatorTests.swift
+//  Pursuit-iOS-CalculatorTests
+//
+//  Created by Eric Widjaja on 4/7/20.
+//  Copyright © 2020 Eric Widjaja. All rights reserved.
+//
+
+import XCTest
+@testable import Pursuit_iOS_Calculator
+
+class Pursuit_iOS_CalculatorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
