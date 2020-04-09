d4cab1ab6c2ee2cf4e556ed83c4973ea847bc459
Eric Widjaja <ericwidjaja@pursuit.org>
2020-04-08 21:23:36 -0400

    trying to implement calculation logic

--- Pursuit-iOS-Calculator/ViewController.swift
+++ Pursuit-iOS-Calculator/ViewController.swift
@@ -18,14 +18,27 @@ class ViewController: UIViewController {
     var numbersOnDisplay: Double = 0
     var previousNumbers: Double = 0
     var operation = 0
-    
+    var isPerformingOperation = false
     
     @IBOutlet weak var resultLabel: UILabel!
     
     @IBAction func numbers(_ sender: UIButton) {
         
         let numbersTag = sender.tag
-        resultLabel.text = resultLabel.text! + String(numbersTag - 1)
+        
+        if isPerformingOperation == true {
+            isPerformingOperation = false
+            
+            print(numbersTag)//to check the value
+            resultLabel.text = String(numbersTag - 1)
+            numbersOnDisplay = Double(resultLabel.text!)!
+            
+        } else {
+            print(numbersTag)//to check the value
+            resultLabel.text = resultLabel.text! + String(numbersTag - 1)
+            numbersOnDisplay = Double(resultLabel.text!)!
+        }
+        
     }
     
     
@@ -33,28 +46,61 @@ class ViewController: UIViewController {
         
         let operatorsTag = sender.tag
         
-        //AC = All Clear button pressed
+        //AC = All Clear button pressed (tag=11)
         if operatorsTag == 11 {
             resultLabel.text = String(0)
             numbersOnDisplay = 0
             previousNumbers = 0
             operation = 0
             
+            return //no more operation after return inside this block of 'if ... { ... }
+        }
+        /* division button's tag = 14
+         multiplication button's tag = 15
+         subtraction button's tag = 16
+         addition button's tag = 17
+         equal button's tag = 18
+         */
+        if operatorsTag == 14 {
+            isPerformingOperation = true
+            previousNumbers = Double(resultLabel.text!)!
+            resultLabel.text = "/"
+            operation = operatorsTag
+            
+            
+        } else if operatorsTag == 15 {
+            isPerformingOperation = true
+            previousNumbers = Double(resultLabel.text!)!
+            resultLabel.text = "X"
+            operation = operatorsTag
+            
+        } else if operatorsTag == 16 {
+            isPerformingOperation = true
+            previousNumbers = Double(resultLabel.text!)!
+            resultLabel.text = "-"
+            operation = operatorsTag
+        
+        } else if operatorsTag == 17 {
+            isPerformingOperation = true
+            previousNumbers = Double(resultLabel.text!)!
+            resultLabel.text = "+"
+            operation = operatorsTag
+        
+        } else if operatorsTag == 18 {
+            isPerformingOperation = true
+            
+            resultLabel.text = "="
             
         }
         
-        
-        
     }
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = #colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1)
         resultLabel.text = ""
     }
-
-
+    
+    
 }
 
